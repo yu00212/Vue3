fb8c42ebd7d7133780d629902ab93c048534ef5b
yu00212 <yu2000212@icloud.com>
2023-06-24 18:17:51 +0900

    add：コンポーネント追加

---
+++ src/components/Payment.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>最近の支出</h1>
+    <div class="payment">
+      <label>カレーライス</label>
+      <label>400円</label>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.payment {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  width: 400px;
+  background-color: aliceblue;
+}
+label {
+  font-size: 20px;
+  font-weight: bold;
+
+}
+</style>

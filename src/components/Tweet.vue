375f569a1697816933ffb995cc8a3617d553f5e2
yu00212 <yu2000212@icloud.com>
2023-06-25 02:28:34 +0900

    add： propsとemitでのcomponent繋ぎ込み

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,40 +1,33 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import TweetPostForm from "./TweetPostForm.vue";
+import TweetList from "./TweetList.vue";
 
 const tweets = ref([
     { id: 0, description: 'Hello, world!' },
     { id: 1, description: '2個目のツイートです'}
 ]);
-const inputtingDescription = ref<string>('')
 
-
-const postTweet = () => {
-  const tweet = { id: Math.random(), description: inputtingDescription.value }
+const postTweet = (description: string) => {
+  const tweet = { id: Math.random(), description:description }
   tweets.value.push(tweet)
-  inputtingDescription.value = ''
-  console.log(tweets.value);
 }
 
 const deleteTweet = (id: number) => {
   tweets.value = tweets.value.filter(t => t.id !== id)
 }
 
+
 </script>
 
 <template>
   <div class="container">
     <h1>Tweeter</h1>
-    <div class="form-container">
-      <input  v-model="inputtingDescription" />
-      <button class="save-button" @click="postTweet()">post</button>
-    </div>
+      <TweetPostForm @post-tweet="postTweet" />
     <div class="tweet-container">
       <p v-if="tweets.length <= 0">ツイートがありません。</p>
-      <ul>
-        <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
-          <span>{{ tweet.description }}</span>
-          <button @click="deleteTweet(tweet.id)" class="delete-button">delete</button>
-        </li>
+      <ul v-else>
+        <TweetList :tweets="tweets" :delete-tweet="deleteTweet"/>
       </ul>
     </div>
   </div>
@@ -56,50 +49,6 @@ const deleteTweet = (id: number) => {
   width: 60%;
   margin-bottom: 12px;
   border-radius: 4px;
-}
-
-.tweet-list {
-  list-style: none;
-  margin-bottom: 12px;
-  border-radius: 4px;
-  font-size: 12px;
-  display: flex;
-  justify-content: space-between;
-  background-color: rgb(204, 219, 233);
-  padding: 8px 20px;
-  width: 300px;
-}
-
-.tweet-container {
-
-}
-
-.save-button {
-  color: #fff;
-  font-weight: bold;
-  background-color: #68c9c9;
-  border-radius: 2px;
-  border: none;
-  width: 60px;
-  height: 22px;
-}
-
-.save-button:hover {
-  background-color: #37bdbd;
-}
-
-.delete-button {
-  color: #fff;
-  font-weight: bold;
-  background-color: #c99a68;
-  border-radius: 2px;
-  border: none;
-  width: 60px;
-  height: 22px;
-}
-
-.delete-button:hover {
-  background-color: #ac783f;
 }
 
 input {

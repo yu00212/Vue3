19800a29444c39ce56d00b5fc22dec2c747324b7
yu00212 <yu2000212@icloud.com>
2023-06-25 04:31:43 +0900

    add：削除時のアラート追加

--- src/components/PersonList.vue
+++ src/components/PersonList.vue
@@ -8,8 +8,10 @@ type Props = {
 defineProps<Props>()
 const emit = defineEmits(['delete'])
 
-const onClickDelete = (id: number) => {
-  emit('delete', id)
+const onClickDelete = (id: number, name: string) => {
+  if (confirm('Delete' + name + '?')) {
+    emit('delete', id)
+  }
 }
 </script>
 
@@ -17,7 +19,7 @@ const onClickDelete = (id: number) => {
   <li v-for="person in persons" :key="person.id" class="person-list">
     <span>{{ person.name }}</span>
     <span>age:{{ person.age }}</span>
-    <button @click="onClickDelete(person.id)">
+    <button @click="onClickDelete(person.id, person.name)">
       <span>delete</span>
     </button>
   </li>

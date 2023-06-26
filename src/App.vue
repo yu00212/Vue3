c9700f17a97508836fc5c1a6b318eb7348b38027
yu00212 <yu2000212@icloud.com>
2023-06-26 19:34:50 +0900

    add：ProvideとInject

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,13 @@
 <script setup lang="ts">
-import {computed, ref} from 'vue'
-import CardList from './components/CardList.vue';
-import SecondCardList from './components/SecondCardList.vue';
+import TodoList from './components/TodoList.vue';
+import { provide }from 'vue';
+import { todos } from './useTodo';
 
-const isActiveTabName = ref('1')
-
-const updateTab = (activeName: string) => {
-  isActiveTabName.value = activeName
-}
-
-const currentComponent = computed(() => {
-  switch (isActiveTabName.value) {
-    case '1':
-      return CardList
-    case '2':
-      return SecondCardList
-    default:
-      return CardList
-  }
-})
-
+provide('todos', todos)
 </script>
 
 <template>
-  <div class="tab-changer">
-    <button @click="updateTab('1')">Tab 1</button>
-    <button @click="updateTab('2')">Tab 2</button>
-  </div>
-  <div class="tab-contents">
-<!--    <CardList></CardList>-->
-    <component :is="currentComponent"></component>
-<!--    <CardList v-if="isFirstTab"></CardList>-->
-<!--    <SecondCardList v-if="!isFirstTab"></SecondCardList>-->
-  </div>
+  <TodoList />
 </template>
 
 <style>
@@ -42,10 +17,5 @@ const currentComponent = computed(() => {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-.tab-contents {
-  margin-top: 20px;
-
 }
 </style>
